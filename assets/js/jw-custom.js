7f7b485c5ea654e59322dbca0236318794f5f83a
kaikc <jankyle@gmail.com>
2023-12-29 13:25:59 +0800

    @1229

--- assets/js/jw-custom.js
+++ assets/js/jw-custom.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 	$(".nav-toggler").click(function () {
 		$("nav").toggleClass("nav-open");
+		/*
+		$(".nav-link").toggleClass("animate__animated");
+		$(".nav-link").toggleClass("animate__fadeInDown");
+
+		$("nav").each(function (index) {
+			$(this)
+				.delay(400 * index)
+				.fadeIn(300);
+		});*/
+
+		$(".nav-link").fadeIn();
+		$(".nav-link").fadeIn("slow");
+		$(".nav-link").fadeIn(3000);
 	});
 });
